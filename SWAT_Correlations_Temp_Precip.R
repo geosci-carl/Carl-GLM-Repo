0833b91034b1a9c8561d61b340f16bd727e4a345
Carl Fredrick G. Aquino <daproudpinoy@gmail.com>
2023-05-03 09:35:12 -0400

    analysis: correlations for temperature and precipitation
    updated code for analysis of correlations for temperature and precipitation

--- SWAT_Correlations_Temp_Precip.R
+++ SWAT_Correlations_Temp_Precip.R
@@ -1,1 +1,56 @@
 # Initial repo population.
+rm(list = ls())
+
+## grab PRECIP data ##########
+precip <- read.csv("SWAT Input Master Data Precipitation.csv") ## load in data
+
+## grab TEMP data ##########
+tempmin <- read.csv("SWAT Input Master Data Temperature - Min.csv") ## load in data
+tempmax <- read.csv("SWAT Input Master Data Temperature - Max.csv") ## load in data
+
+# Data: 14975 observations at 45 locations for 3 different attributes
+# precipiation, daily temperature minimum, daily temperature maximum
+
+n = ncol(precip) # iterate over 45 locations
+
+sequence = seq(from=1,to=n,by=1)
+
+#### NON-SPATIAL #####################################################
+# Does precip covary with temperature min and max?
+data <-as.data.frame(matrix(nrow=nrow(precip),ncol=3))
+colnames(data) <- c("precip","tempmin","tempmax")
+
+# start with tempmin
+
+par(mfrow=c(5,9))
+
+for (i in sequence){
+  
+  data[,1] <- precip[,i]
+  data[,2] <- tempmin[,i]
+  data[,3] <- tempmax[,i]
+  
+  base <- 'Location '
+  corr <- round(cor(data$precip,data$tempmin),2)
+  fit <- lm(precip ~ tempmin, data)
+  location <- paste(base,i,'; r=',corr,sep='')
+  
+  plot(tempmin[,i],precip[,i],
+       xlab="Temperature [deg C]",
+       ylab="Precipitation [mm]",
+       main=location
+  )
+  abline(fit, col="red")
+  
+}
+
+#### SPATIAL #####################################################
+# Does precipitation correlate accorss stations?
+
+n = nrow(precip) # iterate over 14975 observations
+
+sequence = seq(from=1,to=n,by=1)
+
+data <-as.data.frame(precip)
+
+data[,1] <- precip[1,]
