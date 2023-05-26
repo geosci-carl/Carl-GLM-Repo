5ea4e5dde1c719402ac6f00a7075249d08ae0335
Carl Fredrick G. Aquino <daproudpinoy@gmail.com>
2023-05-26 13:58:14 -0400

    Update Initial Analysis and create export subfolder
    Create code for plotting and generation of plots for .nc file and for shapefile. Also create subplots for exporting.

--- Daymet_initial_analysis.py
+++ Daymet_initial_analysis.py
@@ -8,24 +8,29 @@ conda create -n spyder-geo
 conda activate spyder-geo
 conda install -c conda-forge spyder-kernels
 conda install -c conda-forge geopandas 
-conda install -c conda-forge fiona 
 conda install -c conda-forge netcdf4
+conda install -c conda-forge xarray
+
 
 """
-
-import netCDF4
+#%%
+import matplotlib.pyplot as plt
 import geopandas as gpd
-#import fiona
+import xarray as xr
 
 
-# load in our nc file
-file = netCDF4.Dataset('daymet_data/11748_1980_prcp.nc')
-prcp = file.variables['prcp'] # grap precipitation
-lat = file.variables["lat"]
-lon = file.variables["lon"]
-latvals = lat[:]
-lonvals = lon[:]
+#%%
 
-# open shapefile and plot it
-gdf = gpd.read_file("shapefiles/DaymetV4_Tiles_Continental_HI_PR.shp") # open it
-gdf.plot() # plot it
+# Open our .nc file
+ds = xr.open_dataset('daymet_data/11748_1980_prcp.nc')
+
+# plot
+plt.contourf(ds['prcp'][364,:,:])
+plt.colorbar()
+plt.savefig("exports/prcp.svg")
+#%%
+# Read the shapefile
+huc4 = gpd.read_file("shapefiles/NHD_H_0410_HU4_Shape/Shape/WBDHU4.shp")
+huc4.plot() # plot it
+plt.savefig("exports/shp.svg")
+
