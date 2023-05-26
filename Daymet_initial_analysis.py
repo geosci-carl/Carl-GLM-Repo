01a9f12aeb14883416a2a97a4db436cdd43f56b6
Carl Fredrick G. Aquino <daproudpinoy@gmail.com>
2023-05-26 09:07:44 -0400

    update folder structure
    create a subfolder for shapefiles

--- Daymet_initial_analysis.py
+++ Daymet_initial_analysis.py
@@ -7,15 +7,24 @@ first create conda environment in miniconda:
 conda create -n spyder-env -y
 conda activate spyder-env
 conda install spyder-kernels netCDF4 -y
+conda install spyder-kernels geopandas -y
 """
 
 import os
 import netCDF4
 import numpy as np
+import matplotlib.pyplot as plt
+import geopandas
 
 # set the correct working directory
 from os import chdir, getcwd
 wd = getcwd()
 chdir(wd)
 
-f = netCDF4.Dataset('daymet_data/11748_1980_prcp.nc')
+# load in our nc file
+file = netCDF4.Dataset('daymet_data/11748_1980_prcp.nc')
+prcp = file.variables['prcp'] # grap precipitation
+lat = file.variables["lat"]
+lon = file.variables["lon"]
+latvals = lat[:]
+lonvals = lon[:]
---
+++ shapefiles/readme.txt
@@ -0,0 +1,1 @@
+this folder contains shapefiles for the GLM project
