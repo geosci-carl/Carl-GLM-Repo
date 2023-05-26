e9b8a3b69d496369a41cfe9f2d0f2fd1093ed78e
Carl Fredrick G. Aquino <daproudpinoy@gmail.com>
2023-05-26 07:14:00 -0400

    Set up Daymet folder structure / Git ignore: nc files
    Set up Daymet folder structure and set up repo to ignore nc files.

--- Daymet_initial_analysis.py
+++ Daymet_initial_analysis.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Initial analysis
+Goal: open .nc file in Python
 
-This is a temporary script file.
+first create conda environment in miniconda:
+conda create -n spyder-env -y
+conda activate spyder-env
+conda install spyder-kernels netCDF4 -y
 """
 
+import os
+import netCDF4
+import numpy as np
+
+# set the correct working directory
+from os import chdir, getcwd
+wd = getcwd()
+chdir(wd)
+
+f = netCDF4.Dataset('daymet_data/11748_1980_prcp.nc')
