0fdf1ab864fccd4e9ca3d787f402fe2c0d9503d6
Carl Fredrick G. Aquino <daproudpinoy@gmail.com>
2023-08-07 15:03:27 -0400

    Fix 'find nearest' function
    Fixed a bug where the 'find nearest' function wouldn't compute the correct locations.

--- Daymet_initial_analysis.py
+++ Daymet_initial_analysis.py
@@ -9,16 +9,17 @@ conda activate spyder-geo
 conda install -c conda-forge spyder-kernels
 conda install -c conda-forge spyder
 conda install -c conda-forge xarray
+conda install -c conda-forge hmmlearn
 
 """
 #%%
 import pandas as pd
 import matplotlib.pyplot as plt
-#import geopandas as gpd
 import xarray as xr
 import numpy as np
 import time
-#import netCDF4
+#from hmmlearn.hmm import GaussianHMM
+
 
 #%% x array - open precip datafiles
 
@@ -351,9 +352,73 @@ print('duration:')
 print(end - start)
 print('MISCHIEF MANAGED!') 
 
+#%% Let's grab precip data from one centroid for all 41 years (1980 - 2021)
+start = time.time()
+
+# we'll choose the portage river, HUC12 41000100502
+MyTileNames_Prcp = [tile1_prcp, tile2_prcp, tile3_prcp, tile4_prcp, tile5_prcp, tile6_prcp]
+MyTileXs         = [ds1a.x, ds2a.x, ds3a.x, ds4a.x, ds5a.x, ds6a.x]
+MyTileYs         = [ds1a.y, ds2a.y, ds3a.y, ds4a.y, ds5a.y, ds6a.y]
+MyTileLats       = [lats_ds1, lats_ds2, lats_ds3, lats_ds4, lats_ds5, lats_ds6]
+MyTileLons       = [lons_ds1, lons_ds2, lons_ds3, lons_ds4, lons_ds5, lons_ds6]
+ 
+mylocation = locations.loc[locations['Code']=='Portage River'] # portage river, HUC12 41000100502
+locationindex = 252 # for portage river
+mylocationname = locations.iloc[locationindex,1]
+
+## Grab precip data for each station
+
+# Let's initialize a dataframe:
+MyYears = list(range(1980,2022,)) # create sequence of years for year loop
+ones_data = np.ones(shape=(len(MyYears)*365,1))*-999
+PrecipData = pd.DataFrame(ones_data)
+PrecipData.columns = [mylocationname]
+
+
+# select the right tile.
+#MyTiles = [11748,11749,11750,11928,11929,11930]
+MyTileIndex = int(mylocation["TileIndex"][locationindex])
+
+# grab the right indices
+MyLatRi = int(mylocation["LatRi"][locationindex])
+MyLatCi = int(mylocation["LatCi"][locationindex])
+MyLonRi = int(mylocation["LonRi"][locationindex])
+MyLonCi = int(mylocation["LonCi"][locationindex])
+
+MyLat = MyTileLats[MyTileIndex][MyLatRi,MyLatCi]
+MyLon = MyTileLons[MyTileIndex][MyLonRi,MyLonCi]
+
+MyLatY = float(MyLat.y)
+MyLonX = float(MyLon.x)
+
+MyIndexX = int(np.where(MyTileXs[MyTileIndex] == MyLonX)[0])
+MyIndexY = int(np.where(MyTileYs[MyTileIndex] == MyLatY)[0])
+
+
+# grab the data
+
+for i in range(len(MyYears)):
+    print('Starting '+str(MyYears[i]))
+    n=0+i*365 # start at the beginning of our time series
+    m=n+365 # end at the 365th day
+    PrecipData['Portage River'][n:m] = MyTileNames_Prcp[MyTileIndex][MyIndexY,MyIndexX,i]
+    
+# plot a time series
+plt.title("Daily Precipitation for Portage River HUC12 1980-2021 ")
+plt.plot(PrecipData.index, PrecipData['Portage River'],'.')    
+plt.xlabel("Day")
+plt.ylabel("Precipiation [mm]")
+
+plt.savefig("exports/Portage_River_Precip.svg")
+    
+end = time.time()
+print('duration:')
+print(end - start)
+print('MISCHIEF MANAGED!')     
+
 #%% Let's grab precip data from each location for all 41 years (1980 - 2021)
 
-start = time.time() # let's time this (Run 1: 5.06 seconds)
+start = time.time() # let's time this (Run 1: 10.514518022537231 seconds)
 
 ## Sum precip across the days of each year for all tiles
 # initialize new summed tiles
@@ -373,6 +438,11 @@ MyTileNames_Tmin = [tile1_tmin, tile2_tmin, tile3_tmin, tile4_tmin, tile5_tmin, 
 MyTileNames_Tmax = [tile1_tmax, tile2_tmax, tile3_tmax, tile4_tmax, tile5_tmax, tile6_tmax]
 MyTileNames_Prcp_Summed = [tile1_prcp_summed, tile2_prcp_summed, tile3_prcp_summed, tile4_prcp_summed, tile5_prcp_summed, tile6_prcp_summed]
 
+MyTileXs         = [ds1a.x, ds2a.x, ds3a.x, ds4a.x, ds5a.x, ds6a.x]
+MyTileYs         = [ds1a.y, ds2a.y, ds3a.y, ds4a.y, ds5a.y, ds6a.y]
+MyTileLats       = [lats_ds1, lats_ds2, lats_ds3, lats_ds4, lats_ds5, lats_ds6]
+MyTileLons       = [lons_ds1, lons_ds2, lons_ds3, lons_ds4, lons_ds5, lons_ds6]
+
 # sum across the days of each year
 
 for i in range(0,len(MyTileNames_Prcp)):
@@ -384,7 +454,6 @@ for i in range(0,len(MyTileNames_Prcp)):
 ones_data = np.ones(shape=(len(MyYears),len(locations)))*-999
 PrecipData = pd.DataFrame(ones_data, columns=locations["Code"])
 MyYears = list(range(1980,2022,)) # create sequence of years for year loop
-PrecipData.index =  MyYears
 
 for n in range(len(locations)):
     # Grab the right station key.
@@ -393,14 +462,24 @@ for n in range(len(locations)):
     
     # select the right tile.
     #MyTiles = [11748,11749,11750,11928,11929,11930]
-    MyTileIndex = locations["TileIndex"][n] 
-        
-    # grab the right indices
-    MyLatRi = locations["LatRi"][n]
-    MyLatCi = locations["LatCi"][n]
+    MyTileIndex = int(locations["TileIndex"][n] )
     
+    # grab the right indices
+    MyLatRi = int(locations["LatRi"][n])
+    MyLatCi = int(locations["LatCi"][n])
+    MyLonRi = int(locations["LonRi"][n])
+    MyLonCi = int(locations["LonCi"][n])
+
+    MyLat = MyTileLats[MyTileIndex][MyLatRi,MyLatCi]
+    MyLon = MyTileLons[MyTileIndex][MyLonRi,MyLonCi]
+
+    MyLatY = float(MyLat.y)
+    MyLonX = float(MyLon.x)
+
+    MyIndexX = int(np.where(MyTileXs[MyTileIndex] == MyLonX)[0])
+    MyIndexY = int(np.where(MyTileYs[MyTileIndex] == MyLatY)[0])
     # grab the summed data
-    PrecipData[MyColumn] = MyTileNames_Prcp_Summed[int(MyTileIndex)][int(MyLatRi),int(MyLatCi),:]
+    PrecipData[MyColumn] = MyTileNames_Prcp_Summed[int(MyTileIndex)][MyIndexY,MyIndexX,:]
 
 # Finally, let's drop columns with nans
 PrecipDataClean = PrecipData.dropna(axis=1)
